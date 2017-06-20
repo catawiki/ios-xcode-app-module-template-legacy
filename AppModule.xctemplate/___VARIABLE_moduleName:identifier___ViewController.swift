fc9934915f385d1393002ac720d8104ff5f50102
Yekaterina <y.patrenina@catawiki.nl>
2017-06-20 16:32:26 +0200

    Fix the formatting

--- AppModule.xctemplate/___VARIABLE_moduleName:identifier___ViewController.swift
+++ AppModule.xctemplate/___VARIABLE_moduleName:identifier___ViewController.swift
@@ -5,16 +5,19 @@ class ___VARIABLE_moduleName___ViewController: UIViewController, ___VARIABLE_mod
     var presenter: ___VARIABLE_moduleName___Presenting!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
 
     }
 }
 
 
 extension ___VARIABLE_moduleName___ViewController {
-    static func from(storyboard:String) -> ___VARIABLE_moduleName___ViewController {
+
+    static func from(storyboard: String) -> ___VARIABLE_moduleName___ViewController {
         let storyboard = UIStoryboard(name: storyboard, bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "___VARIABLE_moduleName___ViewController")
         return vc as! ___VARIABLE_moduleName___ViewController
     }
+
 }
 
