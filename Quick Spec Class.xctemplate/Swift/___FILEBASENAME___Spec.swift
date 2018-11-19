7ae183aed976ce5730838b551bf1905def65a89e
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2018-11-19 16:33:43 +0100

    Added Quick template

---
+++ Quick Spec Class.xctemplate/Swift/___FILEBASENAME___Spec.swift
@@ -0,0 +1,18 @@
+import Foundation
+import Quick
+import Nimble
+@testable import Catawiki
+
+class ___FILEBASENAMEASIDENTIFIER___: QuickSpec {
+    override func spec() {
+        var subject: ___VARIABLE_productName___!
+
+        beforeEach {
+            subject = ___VARIABLE_productName___()
+        }
+
+        afterEach {
+            subject = nil
+        }
+    }
+}
