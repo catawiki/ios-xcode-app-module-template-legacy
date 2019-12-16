5e5dfb178eeb11b05d5e4d7fa82cbc7c9bb44372
Manish Rathi <m.rathi@catawiki.nl>
2019-12-16 17:29:27 +0100

    Minor clean-up

--- AppModuleTests.xctemplate/___VARIABLE_moduleName:identifier___ViewControllerSpec.swift
+++ AppModuleTests.xctemplate/___VARIABLE_moduleName:identifier___ViewControllerSpec.swift
@@ -7,14 +7,13 @@ class ___VARIABLE_moduleName___ViewControllerSpec: QuickSpec {
     override func spec() {
         var subject: ___VARIABLE_moduleName___ViewController!
         var presenter: ___VARIABLE_moduleName___PresentingMock!
-        
+
         beforeEach {
-            let vc: ___VARIABLE_moduleName___ViewController = ___VARIABLE_moduleName___ViewController()
-            subject = vc
+            subject = ___VARIABLE_moduleName___ViewController()
             presenter = ___VARIABLE_moduleName___PresentingMock()
             subject.presenter = presenter
         }
-        
+
         afterEach {
             subject = nil
             presenter = nil
